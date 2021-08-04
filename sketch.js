6db50d1ae4afde0dc8257c93cede3f927f9142fc
rithvik145 <85678993+rithvik145@users.noreply.github.com>
2021-08-04 13:51:02 +0530

    Add files via upload
    t est commit

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 var wall,thickness;
-var car, speed ,weight;
+var bullet, speed ,weight;
 
 
 function setup() {
-  createCanvas(2600,400);
-  speed = random(223,321);
-  weight = random(30,52); 
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500); 
   thickness = random(22,83);
-car = createSprite(50,200,50,50);
+bullet = createSprite(50,200,50,50);
 
-car.velocityX = speed ;
+bullet.velocityX = speed ;
 
-car.shapeColor = color("white");
+bullet.shapeColor = color("white");
 
-wall = createSprite(1500,200,thickness,height/2);
+wall = createSprite(1500,200,60,height/2);
 wall.shapeColor = color(80,80,80);
 
 
@@ -21,22 +21,27 @@ wall.shapeColor = color(80,80,80);
 
 function draw() {
   background("black");  
-  if(wall.x -car.x< (car.width+wall.width)/2)
+  if(hasCollieded( bullet,wall))
   {
-  car.velocityX = 0
-  var deformation = 0.5 * weight * speed * speed/22500
-  if(deformation>180)
+  bullet.velocityX = 0
+  var damage = 0.5 * weight * speed * speed/22500
+  console.log(damage);
+  if(damage>10)
   {
-      car.shapeColor = color(250,0,0)
+      wall.shapeColor = color("green");
   }
-  if(deformation<100 && defomation>100)
+  if(damage<10)
   {
-      bullents.shapeColor = color(230,230,0)
-  }
-  if(deformation<100)
-  {
-      cars.shapeColor = color("red")
+      wall.shapeColor = color("red");
   }
   }
   drawSprites();
+}
+function hasCollieded( bullet,wall){
+bulletRightEdge = bullet.x +bullet.width;
+wallLeftedge = wall.x
+if(bulletRightEdge >= wallLeftedge){
+return true;
+}
+return false;
 }
